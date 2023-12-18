484334eefe3d0693746cdb606b09bda18d36d366
Nick Griffith <nickolansgriffith@gmail.com>
2023-12-18 06:11:46 -0600

    working cl integration

--- Sources/RNA.swift
+++ Sources/RNA.swift
@@ -1,3 +1,4 @@
+import Foundation
 import ArgumentParser
 
 @main
@@ -7,5 +8,30 @@ struct ReactNativeAssist: ParsableCommand {
     
     public func run() {
         print(printHelloWorld ? "HELLO WORLD" : "NOT HELLO WORLD")
+        do {
+            let output = try safeShell("ls -al")
+            print(output)
+        } catch {
+            print("ERROR: \(error)")
+        }
+    }
+    
+    @discardableResult
+    func safeShell(_ command: String) throws -> String {
+        let task = Process()
+        let pipe = Pipe()
+        
+        task.standardOutput = pipe
+        task.standardError = pipe
+        task.arguments = ["-c", command]
+        task.executableURL = URL(fileURLWithPath: "/bin/zsh")
+        task.standardInput = nil
+
+        try task.run()
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let output = String(data: data, encoding: .utf8)!
+        
+        return output
     }
 }
